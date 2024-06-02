f0d83c493922faded69bc091c2f24685a1a0d612
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-06-02 19:13:55 +0500

    custom scrollbar using shadcn ui scroll area

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,18 +1,26 @@
-"use client"
+"use client";
 
 import type { AppProps } from "next/app";
 import { usePathname } from "next/navigation";
 import Sidebar from "@/components/Sidebar/Sidebar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function MainLayout({ children }: AppProps) {
   const pathname = usePathname();
-  const showSidebar = !pathname.includes("/login") && !pathname.includes("/signup");
+  const showSidebar =
+    !pathname.includes("/login") && !pathname.includes("/signup");
 
   return (
     <html lang="en">
       <body>
-        {showSidebar && <Sidebar />}
-        <div className={showSidebar ? "lg:pl-[17rem] pt-[4rem]" : "no-padding"}>{children}</div>
+        <ScrollArea className="h-screen">
+          {showSidebar && <Sidebar />}
+          <div
+            className={showSidebar ? "lg:pl-[17rem] pt-[4rem]" : "no-padding"}
+          >
+            {children}
+          </div>
+        </ScrollArea>
       </body>
     </html>
   );
