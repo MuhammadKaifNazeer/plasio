74fd694af03ef71d562ec819f78c957f894d92d7
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2025-01-12 03:27:09 +0000

    fix

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,11 @@
-import { LoginForm } from "@/components/LoginForm/LoginForm";
+import { SigninForm } from "@/components/SigninForm/SigninForm";
 import React from "react";
 
 const Login = () => {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <LoginForm />
+        <SigninForm />
       </div>
     </>
   );
--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -15,7 +15,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 // Define the LoginForm component
-export function LoginForm() {
+export function SigninForm() {
   // State variables
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
